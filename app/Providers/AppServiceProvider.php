d321fc00e2ee71a69a02a30bfd0dcdadbac3a6bb
Sharfuddin Shawon <sharfuddin.shawon@gmail.com>
2025-02-04 06:49:53 -0600

    docker condif

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,7 +23,6 @@ class AppServiceProvider extends ServiceProvider
         //
         if(config('app.forceHTTPS')) {
             URL::forceScheme('https');
-            $url->formatScheme('https');
         }
     }
 }
